467e9ba1a95a77e8dcbe1b045bd3cfb5143d04c0
Katherine Nguyen <nguyenkat01@yahoo.com>
2020-11-04 13:41:06 -0800

    Blocks color coded to represent past, present and future

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-var today = new Date();
-console.log(today);
-var currentDate = today.getDate() + "/" + (today.getMonth() +1) + "/" + today.getFullYear();
+// Display current date
+var currentDate = moment().format("dddd, MM-DD-YYYY");
+console.log(currentDate);
 document.getElementById("currentDay");
 $("#currentDay").text(currentDate);
 
+// Past, Present, Future
+var rows = $(".rows");
+console.log(rows[0].id)
+var currentTime = parseInt(moment().format("H"));
+console.log(currentTime);
+
+function changeCalendarColor() {
+    $(".rows").each(function(){
+        console.log($(this).attr('id'))
+        var rowHour = parseInt($(this).attr('id'))
+        if (rowHour < currentTime) {
+            $(this).addClass("past");
+        } else if (rowHour === currentTime) {
+            $(this).addClass("present");
+        } else if (rowHour > currentTime) 
+            $(this).addClass("future");
+            console.log(this)
+            
+    })
+}
+changeCalendarColor();
